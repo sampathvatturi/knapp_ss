e3c0972d88293e0313166ff4c26a7b82f09dcadf
Roop Sagarh Talari <talariroopsagar@gmail.com>
2022-12-20 18:00:25 +0530

    packages installed

--- frontend/src/app/apps/auth/login/login.component.ts
+++ frontend/src/app/apps/auth/login/login.component.ts
@@ -1,74 +1,319 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, UntypedFormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+// import { ApiService } from 'src/app/providers/api.service';
+import { DomSanitizer } from '@angular/platform-browser';
+// import { MD5CryptoService } from 'src/app/services/md5.service';
 
 @Component({
-  selector: 'app-login',
+  selector: 'auth-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styles: [
+    `.form-div-padding{
+    padding: 0;
+    padding-right: 2rem;
+    padding-left: 1rem;
+    padding-top: 16rem !important;
+    }
+      .login-form-margin {
+        margin-bottom: 16px;
+      }
+
+      .login-form-forgot {
+        float: right;
+      }
+
+      .login-form-button {
+        width: 100%;
+      }
+    `
+  ]
 })
 export class LoginComponent implements OnInit {
+  data = {
+    clnt_aplcn_nm: "WMS Dreamstep"
+  }
+  settings = {
+    // screen:{'login':true,'forgot':false,'otp':false,'reset':false,'set':false},
+    current_screen: "login"
+    , screen_heading: "Employee Login"
+  }
 
-  validateForm!: FormGroup;
-  responseMessage!: string;
+  loginForm!: FormGroup;
+  forgotForm!: FormGroup;
+  otpForm!: FormGroup;
+  setPaswrdForm!: FormGroup;
 
-  constructor(
-    private fb: UntypedFormBuilder,
-    private route: Router,
-    // private userService: UserService,
-    // private notificationService: NotificationService,
-    // private ngxUiLoaderService: NgxUiLoaderService,
-    // private md5: Md5hashService
+  err_msg: string = '';
+  errmsg: boolean = false;
+  loader: boolean = false;
+
+  usr_id: any;
+
+  otp_rsnd: boolean = false;
+
+  captchID = null;
+  saltKey = null;
+  captchImg: any;
+
+  constructor(public fb: FormBuilder, 
+    // private apiS: ApiService,
+     private router: Router,
+    private domSanitizer: DomSanitizer,
+    // private md5Srvc: MD5CryptoService
   ) { }
 
   ngOnInit(): void {
-    this.validateForm = this.fb.group({
-      email: [null, [Validators.required]],
-      password: [null, [Validators.required]],
-      remember: [true]
-    });
-    this.storageClear();
-  }
-
-  storageClear(): void {
-    localStorage.clear();
-    sessionStorage.clear();
-  }
-
-  signup() {
-    this.route.navigateByUrl('/signup');
-  }
-
-  forgot() {
-    this.route.navigateByUrl('/forgotpwd');
-  }
-  
-  onSubmit(): void {
-    // this.ngxUiLoaderService.start();
-    // const formData = this.validateForm.value;
-    // const obj = {
-    //   email: formData.email,
-    //   password: this.md5.passwordEncrypt(formData.password)
-    // };
-    // this.userService.login(obj).subscribe((response: any) => {
-    //   console.log("response in Login: ", response);
-    //   this.ngxUiLoaderService.stop();
-    //   this.responseMessage = "You are logged-in";
-    //   this.notificationService.createNotification('success', this.responseMessage);
-    //   localStorage.setItem('token', response?.token);
-    //   localStorage.setItem('role', 'admin');
-    //   sessionStorage.setItem('user_data',JSON.stringify(response?.user_data));
-    //   this.route.navigate(['/app']);
-    // }, (error) => {
-    //   this.ngxUiLoaderService.stop();
-    //   if (error?.error?.message) {
-    //     this.responseMessage = error?.error?.message;
-    //   } else if (error?.message) {
-    //     this.responseMessage = error?.message;
-    //   } else {
-    //     this.responseMessage = GlobalConstants.genericError;
-    //   }
-    //   this.notificationService.createNotification('error', this.responseMessage);
+    // this.loginForm = this.fb.group({
+    //   username: [null, [Validators.required]],
+    //   password: [null, [Validators.required]],
+    //   captcha: [null, [Validators.required]],
+    //   // remember: [true]
     // });
+
+    // this.forgotForm = this.fb.group({
+    //   username: [null, [Validators.required]],
+    //   phoneNumber: [null, [Validators.required]],
+    //   captcha: [null, [Validators.required]],
+    // });
+
+    // this.otpForm = this.fb.group({
+    //   otp: [null, [Validators.required]],
+    // });
+
+    // this.setPaswrdForm = this.fb.group({
+    //   newPassword: [null, [Validators.required]],
+    //   CnfrmPassword: [null, [Validators.required]]
+    // });
+
+    // this.getCaptchaText();
   }
+
+
+
+  validateCaptcha = (calbk: any) => {
+    // if (this.loginForm.value['captcha'] && this.loginForm.value['captcha'].trim() != '') {
+    //   calbk(true);
+    // }
+    // else {
+    //   calbk(false);
+    // }
+  }
+
+
+  validateFrgtPswrdCaptcha = (calbk: any) => {
+    // if (this.forgotForm.value['captcha'] && this.forgotForm.value['captcha'].trim() != '') {
+    //   calbk(true);
+    // }
+    // else {
+    //   calbk(false);
+    // }
+  }
+
+
+  doLogin() {
+        this.loader = true;
+
+    // this.validateCaptcha((cptch_res) => {
+    //   if (cptch_res) {
+    //     this.loader = true;
+    //     this.errmsg = false;
+    //     this.err_msg = null;
+    //     this.loginForm.value.app = 'web';
+    //     localStorage.clear();
+    //     var req_body = {
+    //       username: '' + this.loginForm.value.username,
+    //       password: this.getEncpryPwd(this.loginForm.value.password, this.saltKey),
+    //       app: 'web',
+    //       captcha: this.loginForm.value.captcha.trim(),
+    //       captchaID: this.captchID
+    //     }
+    //     this.apiS.create(req_body, 'auth2/admin/login').subscribe((res) => {
+    //       if (res['status'] == 200) {
+    //         this.loader = false;
+    //         this.errmsg = false;
+    //         if (res['data']) {
+    //           this.errmsg = false;
+    //           this.loader = false;
+    //           localStorage.setItem('userdata', JSON.stringify(res['data'].user));
+    //           localStorage.setItem('clients', JSON.stringify(res['data'].clnts));
+    //           localStorage.setItem('homeUrl', `${res['data'].assignedProfiles.mnu_home_pg}`);
+    //           this.router.navigateByUrl(res['data'].assignedProfiles.mnu_home_pg, { replaceUrl: true });
+    //         } else {
+    //           this.getCaptchaText();
+    //           this.loginForm['captcha'] = '';
+    //           this.errmsg = true;
+    //           this.loader = false;
+    //           this.err_msg = "Incorrect username or password.";
+    //         }
+    //       } else if (res['status'] == 411 || res['status'] == 601) {
+    //         this.errmsg = true;
+    //         this.loader = false;
+    //         this.err_msg = res['message'];
+    //       } else {
+    //         this.getCaptchaText();
+    //         this.loginForm['captcha'] = '';
+    //         this.errmsg = true;
+    //         this.loader = false;
+    //         this.err_msg = res['message'];
+    //       }
+    //     }, (err) => {
+    //       this.getCaptchaText();
+    //       this.loginForm['captcha'] = '';
+    //       this.errmsg = true;
+    //       this.loader = false;
+    //       this.err_msg = "Something went wrong. Please contact the administrator.";
+    //     })
+    //   }
+    //   else {
+    //     this.errmsg = true;
+    //     this.loader = false;
+    //     this.err_msg = "Please enter valid captcha";
+    //   }
+    // })
+
+  }
+
+
+  frgtPasswrd() {
+    this.settings.current_screen = 'forgot';
+    this.settings.screen_heading = "RECOVER YOUR PASSWORD";
+    // this.lgnVw = false;
+    // this.frgtVw = true;
+    // this.rstVw = false;
+    // this.setPVw = false;
+    this.getCaptchaText();
+
+  }
+  bckToLgn() {
+    this.settings.current_screen = 'login';
+    this.settings.screen_heading = "EMPLOYEE LOGIN";
+    // this.lgnVw = true;
+    // this.frgtVw = false;
+    // this.rstVw = false;
+    // this.setPVw = false;
+    this.getCaptchaText();
+  }
+  otpID: any = null;
+  otpsnd() {
+    // this.validateFrgtPswrdCaptcha((cptch_res) => {
+    //   if (cptch_res) {
+    //     this.err_msg = null;
+    //     this.otp_rsnd = false;
+
+    //     let data = {
+    //       phonenumber: this.forgotForm.value.phoneNumber,
+    //       username: this.forgotForm.value.username,
+    //       captcha: this.forgotForm.value.captcha.trim(),
+    //       captchaID: this.captchID
+    //     };
+    //     this.errmsg = false;
+    //     this.loader = true;
+    //     localStorage.clear();
+    //     this.apiS.post(data, 'auth2/admin/forgot-password/send-otp').subscribe((res) => {
+    //       this.errmsg = false;
+    //       this.loader = false;
+    //       if (res && res['status'] == 200) {
+    //         this.otpID = res['data'].otpID;
+    //         // this.lgnVw = false;
+    //         // this.frgtVw = false;
+    //         this.settings.current_screen = 'otp';
+    //         this.settings.screen_heading = "ENTER OTP"
+    //         // this.rstVw = true;
+    //         // this.setPVw = true;
+    //         this.settings.current_screen = 'reset';
+    //         this.settings.screen_heading = "SET YOUR PASSWORD"
+    //         this.otp_rsnd = true;
+    //         // this.shwrcvrydiv = false;
+    //         // this.shwOtpdiv = true;
+    //       } else if (res['status'] == 411 || res['status'] == 601) {
+    //         this.errmsg = true;
+    //         this.err_msg = res['message'];
+    //       } else {
+    //         this.getCaptchaText();
+    //         this.forgotForm['captcha'] = '';
+    //         this.errmsg = true;
+    //         this.err_msg = res['message'];
+    //       }
+    //     }, (err) => {
+    //       this.getCaptchaText();
+    //       this.forgotForm['captcha'] = '';
+    //       this.loader = false;
+    //       this.errmsg = true;
+    //       this.err_msg = "Something went wrong. Please contact the administrator.";
+    //     })
+    //   }
+    //   else {
+    //     this.errmsg = true;
+    //     this.loader = false;
+    //     this.err_msg = "Please enter valid captcha";
+    //   }
+    // })
+
+  }
+
+
+  submtOtp() {
+    // this.loader = true;
+    // var data = {
+    //   phonenumber: this.forgotForm.value.phoneNumber,
+    //   otp: this.otpForm.value.otp,
+    //   username: this.forgotForm.value.username,
+    //   otpID: this.otpID,
+    //   password: this.setPaswrdForm.value.newPassword
+    // }
+    // if (this.setPaswrdForm.value.newPassword == this.setPaswrdForm.value.CnfrmPassword) {
+    //   this.apiS.post(data, 'auth2/admin/forgot-password/validate-otp').subscribe((res) => {
+    //     this.loader = false;
+    //     if (res['status'] == 200) {
+    //       this.settings.current_screen = 'login';
+    //       this.settings.screen_heading = "Employee Login";
+    //       // this.lgnVw = true;
+    //       // this.frgtVw = false;
+    //       // this.rstVw = false;
+    //       // this.setPVw = false;
+    //     } else {
+    //       this.errmsg = true;
+    //       this.err_msg = res['message'];
+    //     }
+    //   }, (err) => {
+    //     this.loader = false;
+    //     this.errmsg = true;
+    //     this.err_msg = "Something went wrong. Please contact the administrator.";
+    //   })
+    // } else {
+    //   this.loader = false;
+    //   this.errmsg = true;
+    //   this.err_msg = "Password doesn't match";
+    //   this.setPaswrdForm.reset();
+    // }
+  }
+
+  getCaptchaText = () => {
+    this.errmsg = false;
+    // this.apiS.get('auth2/admin/login/captcha').subscribe((res) => {
+    //   if (res['status'] == 200 && res['data']) {
+    //     this.captchID = res['data']['cptch_id'];
+    //     this.saltKey = res['data']['salt_ky'];
+    //     this.captchImg = this.domSanitizer.bypassSecurityTrustUrl(res['data']['data']);
+    //   }
+    //   else {
+    //     this.errmsg = true;
+    //     this.err_msg = res['message'];
+    //     this.captchID = null;
+    //     this.saltKey = null;
+    //     this.captchImg = null;
+    //   }
+    // }, (err) => {
+    //   this.captchID = null;
+    //   this.saltKey = null;
+    //   this.captchImg = null;
+    // })
+  }
+
+
+  getEncpryPwd = (pwd: any, sltKy: any) => {
+    // return this.md5Srvc.getMD5EncryptTxt(`${this.md5Srvc.getMD5EncryptTxt(pwd)}${sltKy}`);
+  }
+
 }
