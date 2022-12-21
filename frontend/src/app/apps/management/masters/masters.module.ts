5cd55f41af260d1098ef67fde61ca60cba7a963f
Roop Sagarh Talari <talariroopsagar@gmail.com>
2022-12-21 18:11:53 +0530

    works changes

--- frontend/src/app/apps/management/masters/masters.module.ts
+++ frontend/src/app/apps/management/masters/masters.module.ts
@@ -7,7 +7,65 @@ import { WorksComponent } from './works/works.component';
 import { InventoryComponent } from './inventory/inventory.component';
 import { UomComponent } from './uom/uom.component';
 import { DxButtonModule, DxDataGridModule } from 'devextreme-angular';
-
+import { NzAnchorModule } from 'ng-zorro-antd/anchor';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzBadgeModule } from 'ng-zorro-antd/badge';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCalendarModule } from 'ng-zorro-antd/calendar';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzListModule } from 'ng-zorro-antd/list';
+import { NzMessageModule } from 'ng-zorro-antd/message';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzPaginationModule } from 'ng-zorro-antd/pagination';
+import { NzProgressModule } from 'ng-zorro-antd/progress';
+import { NzRadioModule } from 'ng-zorro-antd/radio';
+import { NzRateModule } from 'ng-zorro-antd/rate';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
+import { NzTagModule } from 'ng-zorro-antd/tag';
+import { NzTimelineModule } from 'ng-zorro-antd/timeline';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
+import { NzUploadModule } from 'ng-zorro-antd/upload';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+const antdModule = [
+  NzButtonModule,
+  NzCardModule,
+  NzAvatarModule,
+  NzRateModule,
+  NzBadgeModule,
+  NzProgressModule,
+  NzRadioModule,
+  NzTableModule,
+  NzDropDownModule,
+  NzTimelineModule,
+  NzTabsModule,
+  NzTagModule,
+  NzListModule,
+  NzCalendarModule,
+  NzToolTipModule,
+  NzFormModule,
+  NzModalModule,
+  NzSelectModule,
+  NzUploadModule,
+  NzInputModule,
+  NzPaginationModule,
+  NzDatePickerModule,
+  NzCheckboxModule,
+  NzMessageModule,
+  NzAnchorModule,
+  DragDropModule,
+  ScrollingModule,
+  NzDrawerModule
+]
 
 @NgModule({
   declarations: [
@@ -21,6 +79,9 @@ import { DxButtonModule, DxDataGridModule } from 'devextreme-angular';
     MastersRoutingModule,
     DxDataGridModule,
     DxButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ...antdModule
   ]
 })
 export class MastersModule { }
--- frontend/src/app/apps/management/masters/works/works.component.ts
+++ frontend/src/app/apps/management/masters/works/works.component.ts
@@ -8,10 +8,24 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class WorksComponent implements OnInit {
   permissions = { "slct_in": 1, "insrt_in": 1, "updt_in": 1, "dlte_in": 1, "exprt_in": 1 };
-  rowData: any = [];
+  rowData: any = [
+    {
+      sno:1,
+      work_name:'parks'
+    },
+    {
+      sno:2,
+      work_name:'schools'
+    },
+    {
+      sno:3,
+      work_name:'hospitals'
+    }
+  ];
   worksForm!: FormGroup;
   showForm: boolean = false;
-
+  drawerTitle = '';
+  submit:boolean = true;
   constructor( private fb: FormBuilder) { }
   columnDefs = [
     { headerName: 'Sl.No.', field: 'sno', alignment: 'center', filter: false },
@@ -35,10 +49,13 @@ export class WorksComponent implements OnInit {
   }
   openSideBar() {
     this.showForm = true;
+    this.drawerTitle = 'Add Work';
+    this.submit= true;
     this.addform();
   }
   openSideBarEdit(role:any,data:any){
       this.showForm = true;
+    this.drawerTitle = 'Edit Work';
       this.editform(data);
   }
   
@@ -52,5 +69,20 @@ export class WorksComponent implements OnInit {
       work_name: ['', [Validators.required]],
     });
   }
+  close(): void {
+    this.showForm = false;
+  }
+  submitForm(): void {
+    if(this.worksForm.valid){
+      console.log('submit', this.worksForm.value);
+    }
+  }
+  updateForm(): void {
+    if(this.worksForm.valid){
+      console.log('submit', this.worksForm.value);
+    }
+  }
+  
+
 
 }
